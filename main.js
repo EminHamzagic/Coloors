fac47e4ad2894c200654af6b342b5949532d8de4
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-01-04 13:46:09 +0100

    work in progress

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
-const sliders = document.querySelectorAll('input[type="range]"')
+const sliders = document.querySelectorAll('input[type="range"]')
 const currentHexes = document.querySelectorAll('.color h2')
 let initaialColors;
+
+
+sliders.forEach((slider) => {
+    slider.addEventListener('input', hslControls);
+})
+
+colorDivs.forEach((div, index) => {
+    div.addEventListener('change', () => {
+        updateTextUI(index);
+    })
+})
 
 
 function generateHex(){
@@ -11,13 +22,25 @@ function generateHex(){
 }
 
 function randomColors(){
+    initaialColors = [];
     colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
+
+        initaialColors.push(chroma(randomColor).hex());
+
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
         checkTextContrast(randomColor, hexText);   
+
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input') 
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+        colorizeSliders(color, hue, brightness, saturation);
     });
+    resetInputs();
 }
 
 function checkTextContrast(color, text){
@@ -28,6 +51,59 @@ function checkTextContrast(color, text){
     else {
         text.style.color = 'white';
     }
+}
+
+function colorizeSliders(color, hue, brightness, saturation){
+    const noSat = color.set('hsl.s', 0);
+    const fullSat = color.set('hsl.s', 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    const midBright = color.set('hsl.l', 0.5);
+    const sacleBright = chroma.scale(['black', midBright, 'white']);
+
+
+
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${sacleBright(0)}, ${sacleBright(0.5)}, ${sacleBright(1)})`;
+    hue.style.backgroundImage = 'linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))'; 
+}
+
+function hslControls(e){
+    const index =
+    e.target.getAttribute('data-bright') ||
+    e.target.getAttribute('data-sat') ||
+    e.target.getAttribute('data-hue');
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    const bgColor = initaialColors[index];
+
+    let color = chroma(bgColor).set('hsl.s', saturation.value).set('hsl.l', brightness.value).set('hsl.h', hue.value);
+    colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUI(index){
+    const activeDiv = colorDivs[index];
+    const color = chroma(activeDiv.style.backgroundColor);
+    const textHex = activeDiv.querySelector('h2');
+    const icons = activeDiv.querySelectorAll('.controls button');
+    textHex.innerText = color.hex();
+    checkTextContrast(color, textHex);
+    for (icon of icons){
+        checkTextContrast(color, icons);
+    }
+}
+
+function resetInputs(){
+    const sliders = document.querySelectorAll('.sliders input')
+    sliders.forEach((slider) => {
+        if (slider.name === 'hue'){
+            const hueColor = initaialColors[slider.getAttribute('data-hue')];
+            const hueValue = chroma(hueColor).hsl()[0];
+            slider.value = Math.floor(hueValue);
+        }
+    })
 }
 
 randomColors();
